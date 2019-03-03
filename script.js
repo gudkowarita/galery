42960a79d1f5dac0820232377b9787e7689d59b7
gudkowarita <42046544+gudkowarita@users.noreply.github.com>
2019-03-03 12:32:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-﻿
 
-alert ('hello world');
 
 function o(i) {
 return typeof i == 'object' ? i : document.getElementById(i);
@@ -21,7 +19,7 @@ return objects
 }
 
 
-document.onready = function() { s('page2').display='block'}
+document.onready = function() { s('page2').display='block';s('name_div').display='block'; s('email_div').display='none';}
 
 var current = 'page2';
 function show(id) {
@@ -30,3 +28,37 @@ o(id).style.display = 'block';
 current = id;
 }
 
+var thisForm = 'name_div';
+function next(id) {
+	o(thisForm).style.display='none';
+	o(id).style.display = 'block';
+}
+
+function validate(form) {
+	fail = validateName (form.name.value)
+	fail += validateEmail (form.email.value)
+	if (fail == "") return true
+	else { alert(fail); return false }
+}
+
+function validateName(field) {
+	return (field =="") ? "Name wasnt entered" : ""
+}
+
+function validateEmail(field) {
+	if (field == "" return "Email wasnt entered"
+	else if (!((field.indexOf(".")>0) &&
+		   (field.indexOf("@")>0)) ||
+		   /[^a-zA-Z0-9.@_-]/.test(field))
+		return "Email is in wrong format"
+	return ""
+}
+
+
+
+
+$(document).ready(function(){
+
+$('#num1, #num2, #num3, #num4').click(function(){$('#num1, #num2, #num3, #num4').removeClass('current_page'); $(this).addClass('current_page')});
+
+});
