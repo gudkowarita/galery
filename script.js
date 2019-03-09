4d9301121e24154a083ceedf090c0db837a4e371
gudkowarita <42046544+gudkowarita@users.noreply.github.com>
2019-03-09 18:31:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,19 @@ return objects
 }
 
 
-document.onready = function() { s('page2').display='block';s('name_div').display='block'; s('email_div').display='none';}
+document.onready = function() { 
+	s('page2').display='block';
+	s('name_div').display='block'; 
+	s('email_div').display='none';
+	
+	var z = 5;				//there's code for likes
+	o('count_likes1').innerHTML = z;
+	o('like1').onclick = function(object)
+		{z+= 1;
+		o('count_likes1').innerHTML = z;}
+
+
+}   //end of function for document ready
 
 var current = 'page2';
 function show(id) {
@@ -57,14 +69,30 @@ function validateEmail(field) {
 
 
 
-
 $(document).ready(function(){
 
 $('li, li>a').mouseover(function(){$(this).css("opacity","0.67")})
 	     .mouseout(function(){$(this).css("opacity","1")});
 $('#num1, #num2, #num3, #num4').click(function(){$('#num1, #num2, #num3, #num4').removeClass('current_page'); $(this).addClass('current_page')});
 $('#menu').click(function(){$('.list').toggle()});
-$('#more_in_header').click(function(){$('.modal').css('display','block')});
-$('.modal').click(function(){$('.modal').css('display','none')});
-	
+$('#more_in_header').click(function(){$('.modal').css('display','block');
+				      $('div:not(".modal")').css('opacity','0.6');
+				      $('.general_heading, .highlight').css('opacity','0.1')});
+//$('.modal:not(:has(.modal_content))').click(function(){$('.modal').css('display','none');
+$('.modal').click(function clo(){$('.modal').css('display','none');
+			     $('div:not(".modal")').css('opacity','1');
+			     $('.general_heading, .highlight').css('opacity','0.9')});
+$('.like').click(function(){$('.modal').stop(clo())});
+//$('div:not()').click(function(){$('.modal').css('display','none');
+//			     $('div:not(".modal")').css('opacity','1');
+//			     $('.general_heading, .highlight').css('opacity','0.9')});
+$('.like').mouseover(function(){$(this).html(this.src="pics/hover_like.png")});
+$('.like').mouseout(function(){$(this).html(this.src="pics/like.png")});
+$('.like').click(function(){$(this).html(this.src="pics/active_like.png");
+				$(this).mouseout(function(){$(this).html(this.src="pics/active_like.png")});
+			    alert('Спасибо за лайк :3')});
+
 });
+
+//$.noConflict();
+//место для lightBox из другой библиотеки
